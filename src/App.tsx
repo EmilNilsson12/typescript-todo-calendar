6b074d3839ae10b94a1a72f01108cafd216c6063
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-09-30 14:37:53 +0200

    Add localStorage saving

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { FunctionComponent, useEffect, useState } from 'react';
 import './App.css';
 import AllTodos from './Components/AllTodos';
 import Calendar from './Components/Calendar';
@@ -7,14 +8,50 @@ import DailyTodos from './Components/DailyTodos';
 // AllTodos
 // Calendar
 
-function App() {
+interface Todo {
+	title: String;
+}
+
+interface TodoArr {
+	todos: Array<Todo>;
+}
+
+const App: FunctionComponent = () => {
+	const [todos, setTodos] = useState<TodoArr['todos']>([]);
+
+	// Initial assignment
+	useEffect(() => {
+		if (!localStorage.getItem('todos')) {
+			localStorage.setItem('todos', JSON.stringify(todos));
+		} else {
+			const lsString = localStorage.getItem('todos');
+			const lsParsed = JSON.parse(lsString || '[]');
+			setTodos(lsParsed);
+		}
+	}, []);
+
+	useEffect(() => {
+		console.log('todos updated');
+		console.log('todos: ', todos);
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
+
+	const handleClick = () => {
+		console.log('Button clicked');
+		const copyOfTodos: TodoArr['todos'] = [...todos, { title: 'hello' }];
+		console.log('copyOfTodos: ', copyOfTodos);
+		setTodos(copyOfTodos);
+		localStorage.setItem('todos', JSON.stringify(todos));
+	};
+
 	return (
 		<div className='App container'>
-			<AllTodos />
+			<button onClick={handleClick}>Add todo</button>
+			<AllTodos todos={todos} />
 			<Calendar />
 			<DailyTodos />
 		</div>
 	);
-}
+};
 
 export default App;
--- src/Components/AllTodos.tsx
+++ src/Components/AllTodos.tsx
@@ -1,5 +1,27 @@
-function AllTodos() {
-	return <div className='AllTodos'>AllTodos...</div>;
+// import { FunctionComponent } from 'react';
+
+interface Todo {
+	title: String;
 }
+
+interface Props {
+	todos: Array<Todo>;
+	// alt
+	// todos: Todo[];
+}
+
+const AllTodos = (props: Props) => {
+	const { todos } = props;
+	console.log('todos: ', todos);
+	console.log('todos length: ', todos.length);
+
+	return (
+		<div className='AllTodos'>
+			{todos.map((todo, i) => (
+				<div key={i}>Title: {todo.title}</div>
+			))}
+		</div>
+	);
+};
 
 export default AllTodos;
