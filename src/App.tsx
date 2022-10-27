6642e93d1f7098c28f1028a1204d2412f096b4b5
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-10-27 09:59:42 +0200

    Update autofill items

--- src/App.tsx
+++ src/App.tsx
@@ -10,145 +10,145 @@ import moment, { Moment } from 'moment';
 
 import { Todo } from './types';
 interface TodoArr {
-	todos: Array<Todo>;
+  todos: Array<Todo>;
 }
 
 const App: FunctionComponent = () => {
-	const [todos, setTodos] = useState<TodoArr['todos']>([
-		{
-			title: 'Interview with Emil',
-			description: '',
-			deadline: '2022-11-01T09:26:23.673Z',
-			completed: true,
-			id: 'e1a17603-bda9-434f-974a-36ebc9b9b924',
-		},
-		{
-			title: 'Check Emils Github',
-			description: '',
-			deadline: '2022-10-28T09:26:11.289Z',
-			completed: false,
-			id: 'c42914d7-77c4-45b3-88ef-f4a196a1c95a',
-		},
-		{
-			title: 'Weekend!',
-			description: '',
-			deadline: '2022-10-29T09:27:23.570Z',
-			completed: false,
-			id: '61fe12a4-c115-4407-8e05-0bcb2199f40f',
-		},
-	]);
-	const [currentDayInFocus, setCurrentDayInFocus] = useState(() => moment());
+  const [todos, setTodos] = useState<TodoArr['todos']>([
+    {
+      title: 'Interview with Emil',
+      description: '',
+      deadline: '2022-10-31T09:26:23.673Z',
+      completed: false,
+      id: 'e1a17603-bda9-434f-974a-36ebc9b9b924',
+    },
+    {
+      title: 'Check Emils Github',
+      description: '',
+      deadline: '2022-10-28T09:26:11.289Z',
+      completed: false,
+      id: 'c42914d7-77c4-45b3-88ef-f4a196a1c95a',
+    },
+    {
+      title: 'Weekend!',
+      description: '',
+      deadline: '2022-10-29T09:27:23.570Z',
+      completed: false,
+      id: '61fe12a4-c115-4407-8e05-0bcb2199f40f',
+    },
+  ]);
+  const [currentDayInFocus, setCurrentDayInFocus] = useState(() => moment());
 
-	const updateMomentObjCallback = (newObj: Moment) => {
-		setCurrentDayInFocus(newObj);
-	};
+  const updateMomentObjCallback = (newObj: Moment) => {
+    setCurrentDayInFocus(newObj);
+  };
 
-	// Initial assignment
-	useEffect(() => {
-		if (!localStorage.getItem('todos')) {
-			localStorage.setItem('todos', JSON.stringify(todos));
-		} else {
-			const lsString = localStorage.getItem('todos');
-			const lsParsed = JSON.parse(lsString || '[]');
-			setTodos(lsParsed);
-		}
-	}, []);
+  // Initial assignment
+  useEffect(() => {
+    if (!localStorage.getItem('todos')) {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    } else {
+      const lsString = localStorage.getItem('todos');
+      const lsParsed = JSON.parse(lsString || '[]');
+      setTodos(lsParsed);
+    }
+  }, []);
 
-	useEffect(() => {
-		localStorage.setItem('todos', JSON.stringify(todos));
-	}, [todos]);
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
-	const handleTodoAdd = (todoObj: Todo) => {
-		console.log('Button clicked');
-		const copyOfTodos: TodoArr['todos'] = [...todos, todoObj];
-		console.log('copyOfTodos: ', copyOfTodos);
-		setTodos(copyOfTodos);
-		localStorage.setItem('todos', JSON.stringify(todos));
-	};
+  const handleTodoAdd = (todoObj: Todo) => {
+    console.log('Button clicked');
+    const copyOfTodos: TodoArr['todos'] = [...todos, todoObj];
+    console.log('copyOfTodos: ', copyOfTodos);
+    setTodos(copyOfTodos);
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
 
-	const crudOperations = {
-		addTodo: (todoObj: Todo) => {
-			// Make copy of todos array
-			const copyOfTodos = [...todos];
+  const crudOperations = {
+    addTodo: (todoObj: Todo) => {
+      // Make copy of todos array
+      const copyOfTodos = [...todos];
 
-			customHookSetTodos(copyOfTodos, setTodos, todoObj);
-		},
-		deleteTodo: (id: String) => {
-			// Make copy of todos array
-			// and filter out the deleted todo
-			const copyOfTodos = [...todos].filter((todo) => todo.id !== id);
+      customHookSetTodos(copyOfTodos, setTodos, todoObj);
+    },
+    deleteTodo: (id: String) => {
+      // Make copy of todos array
+      // and filter out the deleted todo
+      const copyOfTodos = [...todos].filter((todo) => todo.id !== id);
 
-			// Replace todos with copyOfTodos
-			customHookSetTodos(copyOfTodos, setTodos, { skipMe: true });
-		},
-		toggleCompleteTodo: ({ id }: Todo) => {
-			// Find relevant entry from saved todos
-			const todoToBeUpdated = todos.find((todo) => todo.id === id);
+      // Replace todos with copyOfTodos
+      customHookSetTodos(copyOfTodos, setTodos, { skipMe: true });
+    },
+    toggleCompleteTodo: ({ id }: Todo) => {
+      // Find relevant entry from saved todos
+      const todoToBeUpdated = todos.find((todo) => todo.id === id);
 
-			if (todoToBeUpdated) {
-				// Update completed-property to opposite of its inital value
-				todoToBeUpdated.completed = !todoToBeUpdated.completed;
-			}
+      if (todoToBeUpdated) {
+        // Update completed-property to opposite of its inital value
+        todoToBeUpdated.completed = !todoToBeUpdated.completed;
+      }
 
-			// Make copy of todos array
-			// and filter out the deleted todo
-			const copyOfTodos = [...todos].filter((todo) => todo.id !== id);
+      // Make copy of todos array
+      // and filter out the deleted todo
+      const copyOfTodos = [...todos].filter((todo) => todo.id !== id);
 
-			customHookSetTodos(copyOfTodos, setTodos, todoToBeUpdated);
-		},
-		updateTodo: (updatedTodoObj: Todo) => {
-			// Make copy of todos array
-			// and filter out the deleted todo
-			const copyOfTodos = [...todos].filter(
-				(todo) => todo.id !== updatedTodoObj.id
-			);
+      customHookSetTodos(copyOfTodos, setTodos, todoToBeUpdated);
+    },
+    updateTodo: (updatedTodoObj: Todo) => {
+      // Make copy of todos array
+      // and filter out the deleted todo
+      const copyOfTodos = [...todos].filter(
+        (todo) => todo.id !== updatedTodoObj.id
+      );
 
-			customHookSetTodos(copyOfTodos, setTodos, updatedTodoObj);
-		},
-	};
+      customHookSetTodos(copyOfTodos, setTodos, updatedTodoObj);
+    },
+  };
 
-	return (
-		<div className='App container'>
-			<AllTodos
-				todos={todos}
-				updateMomentObjCallback={updateMomentObjCallback}
-				toggleCompleteTodo={crudOperations.toggleCompleteTodo}
-				beginEdit={crudOperations.updateTodo}
-				deleteTodo={crudOperations.deleteTodo}
-			/>
-			<CalenderView
-				todos={todos}
-				callBack={updateMomentObjCallback}
-				momentObj={currentDayInFocus}
-			/>
-			<TodoForm initialDeadline={currentDayInFocus} addTodo={handleTodoAdd} />
-			<DailyTodos
-				todos={todos.filter(
-					(todo) =>
-						todo.deadline.split('T')[0] ===
-						currentDayInFocus.format('YYYY-MM-DD')
-				)}
-				updateMomentObjCallback={updateMomentObjCallback}
-				currentDayInFocus={currentDayInFocus}
-				toggleCompleteTodo={crudOperations.toggleCompleteTodo}
-				beginEdit={crudOperations.updateTodo}
-				deleteTodo={crudOperations.deleteTodo}
-			/>
-		</div>
-	);
+  return (
+    <div className='App container'>
+      <AllTodos
+        todos={todos}
+        updateMomentObjCallback={updateMomentObjCallback}
+        toggleCompleteTodo={crudOperations.toggleCompleteTodo}
+        beginEdit={crudOperations.updateTodo}
+        deleteTodo={crudOperations.deleteTodo}
+      />
+      <CalenderView
+        todos={todos}
+        callBack={updateMomentObjCallback}
+        momentObj={currentDayInFocus}
+      />
+      <TodoForm initialDeadline={currentDayInFocus} addTodo={handleTodoAdd} />
+      <DailyTodos
+        todos={todos.filter(
+          (todo) =>
+            todo.deadline.split('T')[0] ===
+            currentDayInFocus.format('YYYY-MM-DD')
+        )}
+        updateMomentObjCallback={updateMomentObjCallback}
+        currentDayInFocus={currentDayInFocus}
+        toggleCompleteTodo={crudOperations.toggleCompleteTodo}
+        beginEdit={crudOperations.updateTodo}
+        deleteTodo={crudOperations.deleteTodo}
+      />
+    </div>
+  );
 };
 
 export default App;
 
 function customHookSetTodos(
-	array: Todo[],
-	hookCallback: (array: Todo[]) => void,
-	object: any
+  array: Todo[],
+  hookCallback: (array: Todo[]) => void,
+  object: any
 ) {
-	// Add new version of object to array
-	// unless object is missing
-	if (!object.skipMe) array.push(object);
+  // Add new version of object to array
+  // unless object is missing
+  if (!object.skipMe) array.push(object);
 
-	// Replace todos with copyOfTodos
-	hookCallback(array);
+  // Replace todos with copyOfTodos
+  hookCallback(array);
 }
